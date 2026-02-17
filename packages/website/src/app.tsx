1fc481bd84a67ae60e2413ea88038efa856e3716
JuliusWallblom <juliuswallblom@gmail.com>
2026-02-17 23:16:55 +0100

    fix(router): reset scroll position on client navigation

--- packages/website/src/app.tsx
+++ packages/website/src/app.tsx
@@ -1,10 +1,12 @@
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
 import { Outlet } from "react-router-dom";
+import { NavigationScrollToTop } from "shared/navigation";
 
 export function App() {
   return (
     <div className="min-h-screen bg-background">
+      <NavigationScrollToTop />
       <Header />
       <main>
         <Outlet />
